4508aaddb37471d959b29ad85cf8550a91280463
Matthew Klemmer <101160467+TheKlemmerCode@users.noreply.github.com>
2023-02-05 19:27:12 -0500

    Develop (#2)
    * Created a new endpoint, upgraded endpoint to a websocket, begin listening for messages and echoing back.
    
    * Updated the functionality of the frontend to display messages better.

--- main.go
+++ main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/TheKlemmerCode/go-react-chat/pkg/websocket"
 )
 
+// define WebSocket endpoint
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	// upgrade this connection to a WebSocket
+	// connection
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	// listen indefinitely for new messages coming
+	// through WebSocket connection
+	pool.Register <- client
+	client.Read()
+}
+
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	// map `/ws` endpoint to the `serveWs` function
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
 }
 
 func main() {
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
